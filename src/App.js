99779622be73012cbd8e400f675877507b382ca2
Venkat Ganesan <vgkrish619@gmail.com>
2018-10-31 23:28:50 -0400

    wrong fodler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const App = () => (
-  <div>
-    <p>
-      React app built with parcel, code in github, CICD with CircleCI, hosted on
-      aws.
-    </p>
-  </div>
+  <article className="message">
+    <div className="message-header">
+      <p>Hello World</p>
+      <button className="delete" aria-label="delete" />
+    </div>
+    <div className="message-body">
+      React app built with
+      <strong>parcel, code in github, CICD with CircleCI</strong>, hosted on
+      <em> aws</em>.
+    </div>
+  </article>
 );
 export default App;
